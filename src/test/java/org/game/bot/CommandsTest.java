b71cfe6fa9dbfd5cfd85fca011d9763db499c50c
pavelhardzei <pavelgordei11@gmail.com>
2021-04-28 17:59:08 +0300

    update tests

--- src/test/java/org/game/bot/CommandsTest.java
+++ src/test/java/org/game/bot/CommandsTest.java
@@ -33,18 +33,18 @@ public class CommandsTest {
 
     @Test
     @Order(1)
-    void startCommandTest() throws InvalidCommandFormatException {
-        StartCommand command = new StartCommand(null);
-        var result = command.execute(user1, service);
+    void startCommandTest() {
+        StartCommand command = new StartCommand(service);
+        var result = command.execute(user1, null);
         String resultString = result.get(0).getText();
         assertEquals("Добро пожаловать. Для просмотра списка команд введите /help", resultString);
     }
 
     @Test
     @Order(2)
-    void createRoomCommandTest() throws InvalidCommandFormatException {
-        CreateRoomCommand command = new CreateRoomCommand(null);
-        command.execute(user1, service);
+    void createRoomCommandTest() {
+        CreateRoomCommand command = new CreateRoomCommand(service);
+        command.execute(user1, null);
         assertEquals(1, Room.rooms.size());
         assertEquals(user1, Room.findUser(user1).get().getValue().getUsers().get(0));
         assertFalse(Room.findUser(user1).get().getValue().isInGame());
@@ -52,32 +52,31 @@ public class CommandsTest {
 
     @Test
     @Order(3)
-    void exitCommandTest() throws InvalidCommandFormatException {
-        ExitCommand command = new ExitCommand(null);
-        command.execute(user1, service);
+    void exitCommandTest() {
+        ExitCommand command = new ExitCommand(service);
+        command.execute(user1, null);
         assertFalse(Room.findUser(user1).isPresent());
     }
 
     @Test
     @Order(4)
-    void joinCommandTest() throws InvalidCommandFormatException {
-        CreateRoomCommand createCommand = new CreateRoomCommand(null);
-        createCommand.execute(user1, service);
+    void joinCommandTest() {
+        CreateRoomCommand createCommand = new CreateRoomCommand(service);
+        createCommand.execute(user1, null);
         String key = Room.findUser(user1).get().getKey();
-        JoinCommand command = new JoinCommand(key);
-        command.execute(user2, service);
-        command.execute(user3, service);
+        JoinCommand command = new JoinCommand(service);
+        command.execute(user2, key);
+        command.execute(user3, key);
         assertEquals(3, Room.rooms.get(key).getUsers().size());
     }
 
     @Test
     @Order(5)
-    void startGameCommandTest() throws InvalidCommandFormatException {
+    void startGameCommandTest() {
         String key = Room.findUser(user1).get().getKey();
-        JoinCommand command = new JoinCommand(key);
 
         StartGameCommand startGameCommand = new StartGameCommand(service);
-        startGameCommand.execute(service, user2);
+        startGameCommand.execute(user2, null);
         assertNotNull(Room.rooms.get(key).getLeader());
         assertTrue(Room.rooms.get(key).isInGame());
         assertEquals(0, Room.rooms.get(key).getCurrentLetterIndex());
@@ -85,19 +84,19 @@ public class CommandsTest {
 
     @Test
     @Order(6)
-    void setKeywordCommandTest() throws InvalidCommandFormatException {
+    void setKeywordCommandTest() {
         String key = Room.findUser(user1).get().getKey();
         User leader = Room.rooms.get(key).getLeader();
 
         String word = "абвгдэйка";
-        SetKeywordCommand setKeywordCommand = new SetKeywordCommand(word);
-        setKeywordCommand.execute(leader, service);
+        SetKeywordCommand setKeywordCommand = new SetKeywordCommand(service);
+        setKeywordCommand.execute(leader, word);
         assertEquals(word, Room.rooms.get(key).getKeyword());
     }
 
     @Test
     @Order(7)
-    void makeAssociationCommandTest() throws InvalidCommandFormatException {
+    void makeAssociationCommandTest() {
         String key = Room.findUser(user1).get().getKey();
         User leader = Room.rooms.get(key).getLeader();
 
@@ -108,15 +107,15 @@ public class CommandsTest {
             notLeader = user2;
 
         assertEquals(0, Room.rooms.get(key).getAssociations().size());
-        new MakeAssociationCommand("абц майонез такой").execute(notLeader, service);
+        new MakeAssociationCommand(service).execute(notLeader, "абц майонез такой");
         assertEquals(1, Room.rooms.get(key).getAssociations().size());
-        new MakeAssociationCommand("ерунда какая-то").execute(notLeader, service);
+        new MakeAssociationCommand(service).execute(notLeader, "ерунда какая-то");
         assertEquals(1, Room.rooms.get(key).getAssociations().size());
     }
 
     @Test
     @Order(8)
-    void guessCommandTest() throws InvalidCommandFormatException {
+    void guessCommandTest() {
         String key = Room.findUser(user1).get().getKey();
         User leader = Room.rooms.get(key).getLeader();
 
@@ -131,15 +130,15 @@ public class CommandsTest {
         else
             anotherPlayer = user2;
 
-        new GuessCommand(Room.rooms.get(key).getUsers().indexOf(notLeader) + " абц").execute(leader, service);
+        new GuessCommand(service).execute(leader, Room.rooms.get(key).getUsers().indexOf(notLeader) + " абц");
 
         assertEquals("а", Room.rooms.get(key).getCurrentPrefix());
         assertEquals(0, Room.rooms.get(key).getAssociations().size());
 
-        new MakeAssociationCommand("аррррргх звуки злости").execute(notLeader, service);
+        new MakeAssociationCommand(service).execute(notLeader, "аррррргх звуки злости");
         assertEquals(1, Room.rooms.get(key).getAssociations().size());
-        new GuessCommand(Room.rooms.get(key).getUsers().indexOf(notLeader) + " аррррргх")
-                .execute(anotherPlayer, service);
+        new GuessCommand(service)
+                .execute(anotherPlayer, Room.rooms.get(key).getUsers().indexOf(notLeader) + " аррррргх");
         assertEquals("аб", Room.rooms.get(key).getCurrentPrefix());
         assertEquals(0, Room.rooms.get(key).getAssociations().size());
     }
